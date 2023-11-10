9432d1ea6f52e84e334fbb47cb0727062dc6dec3
Blockchain Spectre <blockchainspectre@gmail.com>
2023-11-10 17:28:15 +0530

    Update without wallet source

--- wallets/transactionrequest.go
+++
@@ -1,9 +0,0 @@
-package wallets
-
-type TransactionRequest struct {
-	SenderPrivateKey           *string `json:"senderPrivateKey"`
-	SenderBlockchainAddress    *string `json:"senderBlockchainAddress"`
-	RecipientBlockchainAddress *string `json:"recipientBlockchainAddress"`
-	SenderPublicKey            *string `json:"senderPublicKey"`
-	Value                      *string `json:"value"`
-}
